f176003f2a38b5e3085f856c7bb3e0434e9c0979
Amir Salar <amirsalar1379@yahoo.com>
2025-06-03 13:01:18 -0700

    cnn

--- cnn/mnist.py
+++ cnn/mnist.py
@@ -17,3 +17,18 @@ testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, 
 # Create data loaders to load the data in batches
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False)
+
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+         
+        # Define the layers
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)  # Conv layer: 1 input channel (grayscale), 32 output channels
+        self.pool = nn.MaxPool2d(2, 2)  # Max pooling layer
+        self.fc1 = nn.Linear(32 * 6 * 6, 10)  # Fully connected layer
+ 
+    def forward(self, x):
+        x = self.pool(torch.relu(self.conv1(x)))  # Apply conv, ReLU activation, and pooling
+        x = x.view(-1, 32 * 6 * 6)  # Flatten the tensor for the fully connected layer
+        x = self.fc1(x)  # Apply the fully connected layer
+        return x
